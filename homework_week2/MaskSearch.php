5a99dc7066b95f58e8078ca85d651d89e2e73a3a
enigma722 <enigma722@gmail.com>
2021-03-11 23:36:44 +0800

    week2 homework

---
+++ homework_week2/MaskSearch.php
@@ -0,0 +1,47 @@
+<?php
+require_once(__DIR__."/util.php");
+require_once(__DIR__.'/../vendor/autoload.php');
+
+/* 取得資料 */
+// 開啟CSV檔; "r" 只讀方式打開，將文件指針指向文件頭。;
+$file = fopen('https://data.nhi.gov.tw/Datasets/Download.ashx?rid=A21030000I-D50001-001&l=https://data.nhi.gov.tw/resource/mask/maskdata.csv', 'r');
+
+// 判斷是否為第一筆為(表頭)
+$isHeader = true;
+
+// 讀取CSV內容; 使用支援UTF-8的__fgetcsv函數，解決亂碼問題
+while ($data = __fgetcsv($file)) {
+    if($isHeader) {
+        // 第一筆為表頭
+        $header = $data;
+        $isHeader = false;
+        
+    } else {
+        // 取出資料所需欄位
+        $row = [$header[1] => $data[1],     // 醫事機構名稱
+                $header[2] => $data[2],     // 醫事機構地址
+                $header[4] => $data[4]];    // 成人口罩剩餘數
+        $records[] = $row;
+    }
+}
+
+// 關閉文件
+fclose($file);
+
+
+/* 處理資料 */
+// 1. 過濾不符合搜尋條件的地址
+$searchKey = $argv[1];
+
+$result = array_filter(
+    $records, 
+    fn($r) => strpos($r["醫事機構地址"], $searchKey) !== false
+);
+
+// 2. 以口罩數量做排序
+usort($result, 
+      fn($r1, $r2) => $r2["成人口罩剩餘數"] - $r1["成人口罩剩餘數"]);
+
+// 3. 使用climate顯示表格
+$climate = new League\CLImate\CLImate;
+$climate->table($result);
